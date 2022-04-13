ce2fa6f5fd999a8a564702529fbecce8ec078da3
Houmed Hassan Mohamed <houmed.hassan@dct.dj>
2022-04-13 18:16:52 +0300

    reservation

--- src/main/java/com/safara/entities/administration/controller/HomController.java
+++ src/main/java/com/safara/entities/administration/controller/HomController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomController {
 
-	@RequestMapping("/")
-	public String index()
-	{
-		return "index.html";
-	}
+//	@RequestMapping("/")
+//	public String index()
+//	{
+//		return "index.html";
+//	}
 }
---
+++ src/main/java/com/safara/web/AccueilController.java
@@ -0,0 +1,14 @@
+package com.safara.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AccueilController {
+
+	@RequestMapping("/")
+	public String index()
+	{
+		return "index.html";
+	}
+}
